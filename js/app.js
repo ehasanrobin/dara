340997aa8cb832c76ebafecc0cf17554aadd3de0
ehasanrobin <ehasanrobin44@gmail.com>
2022-10-20 23:15:32 +0600

    dara website task

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var slider = tns({
+  container: ".my-slider",
+  items: 3,
+  nav: false,
+  controls: false,
+  mouseDrag: true,
+  responsive: {
+    640: {
+      edgePadding: 5,
+      gutter: 30,
+      items: 3,
+    },
+    700: {
+      gutter: 30,
+      items: 4,
+    },
+    900: {
+      items: 6,
+    },
+  },
+});
+
+document.querySelector(".search-btn").addEventListener("click", () => {
+  const navmenu = document.querySelector(".nav-menu");
+  document.querySelector(".input-box").classList.remove("none");
+  document.querySelector(".searchInput").focus();
+  navmenu.classList.add("none");
+});
+
+document.querySelector(".searchInput").addEventListener("blur", () => {
+  const navmenu = document.querySelector(".nav-menu");
+  document.querySelector(".input-box").classList.add("none");
+  navmenu.classList.remove("none");
+});
